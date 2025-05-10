c4ce467d0d12cd461f7b3f07804941d87246d273
spencer <spencermimo@gmail.com>
2025-05-10 20:28:49 +0200

    Initial commit for carsell6

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/artboard.png')} style={styles.logo} />
       <StatusBar style="auto" />
     </View>
   );
